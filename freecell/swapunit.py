53eb39af0b8d6170c28933090a3dd0d59384edab
sh999 <sh999gh@gmail.com>
2017-01-27 17:53:03 -0500

    Working on checking alternative colors in swap

--- freecell/swapunit.py
+++ freecell/swapunit.py
@@ -34,41 +34,80 @@ def ret_movable(stack1,stack2):
 	for suit in expected_suits:
 		expected_cards.extend([str(expected_val)+suit])
 	movable = []  # Movable cards from stack 1
-	for exp_card in expected_cards:
-		for c in range(0,len(stack1)):
-			if stack1[c] == exp_card:
-				movable = stack1[c:]
-				# print movable
-				return True,movable
+	print "Expected cards:",expected_cards
+	'''
+			Loop through each expected card
+				Loop from top to bottom of stack1
+					If card is present, continue looping and look for alternating numbers and colors
+					If looping ends at bottom of stack1, success
+					If can't find card or if looping doesn't end at bottom of stack1, go to the next expected card
+	'''
+	movable_valid = False  # Set to true only once, if we have found movable cards 
+	for expected in expected_cards:
+		# 2 possible expected cards, same value different suits
+		found_expected = False
+		for s1card in stack1:
+			# Look from stack1 top to bottom for this first expected card
+			if expected == s1card:
+				# If it's in stack1, note it. Then set sights on the next possible card. If prev see 4h, next look for 3s or 3c
+				found_expected = True
+				looking_for_val = str(int(expected[0])-1)
+				if expected[1] == "h" or expected[1] == "d":
+					looking_for_suits = ["s","c"]
+				elif expected[1] == "c" or expected[1] == "s":
+					looking_for_suits = ["h","d"]
+
+			if found_expected:
+				# Since we found the expected card previously, 
+				looking_for_val = str(int(expected[0])-1)
+	if found_expected == False and 
+
+
+	# for exp_card in expected_cards:
+	# 	for c in range(0,len(stack1)):
+	# 		if stack1[c] == exp_card:
+	# 			# Found expected card in stack1, no
+	# 			movable.extend([exp_card])
+	# 			# movable = stack1[c:]
+	# 			# print movable
+	# 			return True,movable
 	return False,None
 
 def move_stacks_norestrict(stack1,stack2):
 	'''
 		Move stack1 to stack2 with no restrictions on blanks
 	'''
-	# print ret_movable(stack1,stack2)
-	movable_bool,movable_cards = ret_movable(stack1,stack2)
-	if movable_bool:
-		stack2.extend(movable_cards)
-	return stack2
-	# print stack2
+	if len(stack2) == 0:
+		return stack1,stack2
+	elif len(stack1) == 0:
+		return stack2,stack2
+	else:
+		movable_bool,movable_cards = ret_movable(stack1,stack2)
+		# print len(movable_cards)
+		if movable_bool == False:
+			return stack1,stack2
+		elif movable_bool:
+			stack2.extend(movable_cards)
+			stack1 = [item for item in stack1 if item not in movable_cards]
+			return stack1,stack2
 
 def move_stacks_restrict(stack1,stack2,blanks):
 	'''
 		Move stack1 to stack2 with restrictions on blanks
 	'''
 	if len(stack2) == 0:
-		return stack1
+		return stack1,stack2
 	elif len(stack1) == 0:
-		return stack2
+		return stack2,stack2
 	else:
 		movable_bool,movable_cards = ret_movable(stack1,stack2)
 		# print len(movable_cards)
 		if movable_bool == False:
-			return stack2
+			return stack1,stack2
 		elif len(movable_cards) < blanks and movable_bool:
 			stack2.extend(movable_cards)
-		return stack2
+			stack1 = [item for item in stack1 if item not in movable_cards]
+			return stack1,stack2
 
 def main():
 	blanks = 4 	# Number of blank spots
@@ -100,38 +139,51 @@ def test_move_stacks_restrict():
 	print stack2
 
 class TestSwapFuncs(unittest.TestCase):
-	def test_infinite_blank_swap(self):
+	def test_retmovable_true(self):
 		stack1 = ['9h','Jc','4s','3h','2s']
 		stack2 = ['2d','5h']
-		stack2 = move_stacks_norestrict(stack1,stack2)
-		self.assertEqual(stack2,['2d','5h','4s','3h','2s'])
-		self.assertEqual(stack1,['9h','Jc'])
-	def test_infinite_blank_swap2(self):
+		movable_bool,movable_cards = ret_movable(stack1,stack2)
+		self.assertEqual(movable_bool,True)
+		self.assertEqual(movable_cards,['4s','3h','2s'])
+	def test_retmovable_false(self):
 		stack1 = ['9h','Jc','4s','4h','2s']
 		stack2 = ['2d','5s']
-		stack2 = move_stacks_norestrict(stack1,stack2)
-		self.assertEqual(stack2,['2d','5s','4h','2s'])
+		movable_bool,movable_cards = ret_movable(stack1,stack2)
+		self.assertEqual(movable_bool,False)
+		self.assertEqual(movable_cards,None)
+	def test_nonrestricted_swaptrue(self):
+		stack1 = ['9h','Jc','4s','3h','2s']
+		stack2 = ['2d','5h']
+		stack1,stack2 = move_stacks_norestrict(stack1,stack2)
+		self.assertEqual(stack2,['2d','5h','4s','3h','2s'])
+		self.assertEqual(stack1,['9h','Jc'])
+	def test_nonrestricted_swapfalse(self):
+		stack1 = ['9h','Jc','4s','4h','2s']
+		stack2 = ['2d','5s']
+		stack1,stack2 = move_stacks_norestrict(stack1,stack2)
+		self.assertEqual(stack2,['2d','5s'])
+		self.assertEqual(stack1,['9h','Jc','4s','4h','2s'])
 	def test_restricted_blank_swap2(self):
 		stack1 = ['9h','Jc','4s','4h','2s']
 		stack2 = ['2d','5s']
-		stack2 = move_stacks_restrict(stack1,stack2,4)
+		stack1,stack2 = move_stacks_restrict(stack1,stack2,4)
 		self.assertEqual(stack2,['2d','5s','4h','2s'])
 	def test_swap_to_none(self):
 		stack1 = ['9h','Jc','4s','3h','2s']
 		stack2 = []
-		stack2 =move_stacks_restrict(stack1,stack2,4)
+		stack1,stack2 = move_stacks_restrict(stack1,stack2,4)
 		self.assertEqual(stack2,stack1)
 	def test_blank_origin(self):
 		stack1 = []
 		stack2 = ['2d','5s'] 
 		old_stack2 = stack2
-		stack2 = move_stacks_restrict(stack1,stack2,4)
+		stack1,stack2 = move_stacks_restrict(stack1,stack2,4)
 		self.assertEqual(stack2,old_stack2)
 	def test_swap_does_nothing(self):
 		stack1 = ['2d','5s']
 		stack2 = ['9h','Jc','4s','4h','2s']
 		old_stack2 = stack2
-		stack2 = move_stacks_restrict(stack1,stack2,4)
+		stack1,stack2 = move_stacks_restrict(stack1,stack2,4)
 		self.assertEqual(stack2,old_stack2)
 if __name__ == "__main__":
 
